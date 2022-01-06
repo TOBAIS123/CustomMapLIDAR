97ee3f8cd870855710027592753b3b418d2020ef
TOBAIS123 <erfanyeganehfar@gmail.com>
2022-01-06 17:18:38 -0500

    fix

--- features.py
+++ features.py
@@ -7,82 +7,83 @@ from scipy.odr import *
 from scipy.odr import ODR
 from scipy.odr.odrpack import Model, RealData
 
+
 class featuresDetection:
     def __init__(self):
         # init vars
-        self.EPSILON=10
-        self.DELTA=501
-        self.SNUM=6
-        self.PMIN=20
-        self.GMAX=20
-        self.SEED_SEGEMENTS=[]
-        self.LINE_SEGEMENTS=[]
-        self.LASERPOINTS=[]
-        self.LINE_PARAMS=None
-        self.NP=len(self.LASERPOINTS)-1
-        self.LMIN=20 # min len of line segements
-        self.LR=0 # length of line
-        self.PR=0 # num of pinged points on line
+        self.EPSILON = 10
+        self.DELTA = 501
+        self.SNUM = 6
+        self.PMIN = 20
+        self.GMAX = 20
+        self.SEED_SEGEMENTS = []
+        self.LINE_SEGEMENTS = []
+        self.LASERPOINTS = []
+        self.LINE_PARAMS = None
+        self.NP = len(self.LASERPOINTS)-1
+        self.LMIN = 20  # min len of line segements
+        self.LR = 0  # length of line
+        self.PR = 0  # num of pinged points on line
 
     # distance between 2 points
-    def dist_point2point(self,point1,point2):
-        Px=(point1[0]-point2[0])**2
-        Py=(point1[1]-point2[1])**2
+    def dist_point2point(self, point1, point2):
+        Px = (point1[0]-point2[0])**2
+        Py = (point1[1]-point2[1])**2
         return math.sqrt(Px+Py)
 
     # distance from a point to a line
     def dist_point2line(self, params, point):
-        A,B,C=params
-        distance=abs(A*point[0]+B*point[1]+C)/math.sqrt(A**2+B**2)
+        A, B, C = params
+        distance = abs(A*point[0]+B*point[1]+C)/math.sqrt(A**2+B**2)
         return distance
 
     # find two points from a line equation
-    def line_2points(self,m,b):
-        x=5
-        y=m*x+b
-        x2=2000
-        y2=m*x2+b
-        return [(x,y), (x2,y2)]
+    def line_2points(self, m, b):
+        x = 5
+        y = m*x+b
+        x2 = 2000
+        y2 = m*x2+b
+        return [(x, y), (x2, y2)]
 
     # converts the general form of a line to slope intercept
-    def lineForm_G2SI(self,A,B,C):
-        m=-A/B
-        B=-C/B
-        return m,B
+    def lineForm_G2SI(self, A, B, C):
+        m = -A/B
+        B = -C/B
+        return m, B
 
     # converts the slope intercept of a line to general form
-    def lineForm_Si2G(self,m,B):
-        A,B,C=-m,1,-B
-        if A<0:
-            A,b,C=-A,-B,-C
-        den_a=Fraction(A).limit_denominator(1000).as_integer_ratio()[1]
-        den_c=Fraction(C).limit_denominator(1000).as_integer_ratio()[1]
-        gcd = np.gdc(den_a,den_c)
-        lcm= den_a *den_c / gcd
+    def lineForm_Si2G(self, m, B):
+        A, B, C = -m, 1, -B
+        if A < 0:
+            A, b, C = -A, -B, -C
+        den_a = Fraction(A).limit_denominator(1000).as_integer_ratio()[1]
+        den_c = Fraction(C).limit_denominator(1000).as_integer_ratio()[1]
+        gcd = np.gcd(den_a, den_c)
+        lcm = den_a * den_c / gcd
 
-        A=A*lcm
-        B=B*lcm
-        C=C*lcm
-        return A,B,C
+        A = A*lcm
+        B = B*lcm
+        C = C*lcm
+        return A, B, C
 
     # assuming the lines always intersect
-    def line_interesect_general(self,params1,params2):
-        a1,b1,c1 = params1
-        a2,b2,c2=params2
-        x=(c1*b2-b1*c2)/(b1*a2-a1*b2)
-        y=(a1*c2-a2*c1)/(b1*a2-a1*b2)
-        return x,y
+    def line_interesect_general(self, params1, params2):
+        a1, b1, c1 = params1
+        a2, b2, c2 = params2
+        x = (c1*b2-b1*c2)/(b1*a2-a1*b2)
+        y = (a1*c2-a2*c1)/(b1*a2-a1*b2)
+        return x, y
 
-    def points_21ine (self, point1, point2):
+    def points_21ine(self, point1, point2):
         m, b = 0, 0
         if point2[0] == point1[0]:
             pass
         else:
-            m = (point2[1] - point1[1]) / (point2 [0] - point1[0])
+            m = (point2[1] - point1[1]) / (point2[0] - point1[0])
             b = point2[1] - m * point2[0]
         return m, b
 
-    def projection_point2line (self, point, m, b):
+    def projection_point2line(self, point, m, b):
         x, y = point
         m2 = -1 / m
         c2 = y - m2 * x
@@ -90,123 +91,126 @@ class featuresDetection:
         intersection_y = m2 * intersection_x + c2
         return intersection_x, intersection_y
 
-    def AD2pos (self, distance, angle, robot_position):
-        x = distance * math.cos (angle) + robot_position[0]
+    def AD2pos(self, distance, angle, robot_position):
+        x = distance * math.cos(angle) + robot_position[0]
         y = -distance * math.sin(angle) + robot_position[1]
-        return (int(x). int(y))
+        return (int(x), int(y))
 
     def laser_points_set(self, data):
-        self.LASERPOINTS =[]
+        self.LASERPOINTS = []
         if not data:
             pass
         else:
             for point in data:
-                coordinates = self.AD2pos (point [0], point [1], point [2])
-                self. LASERPOINTS. append ([coordinates, point[1]])
+                coordinates = self.AD2pos(point[0], point[1], point[2])
+                self. LASERPOINTS. append([coordinates, point[1]])
             self.NP = len(self. LASERPOINTS) - 1
 
-
     # Define a function (quadratic in our case) to fit the data with.
-    def linear_func (self, p, x):
+
+    def linear_func(self, p, x):
         m, b = p
         return m * x + b
 
-
     def odr_fit(self, laser_points):
-        x = np.array ([i [0][0] for i in laser_points])
-        y = np.array ([i [0][1] for i in laser_points])
+        x = np.array([i[0][0] for i in laser_points])
+        y = np.array([i[0][1] for i in laser_points])
 
         # create a model for fitting.
         linear_model = Model(self.linear_func)
 
         # create a RealData object using our initiated data from above.
-        data= RealData(x, y)
-        
+        data = RealData(x, y)
+
         # Set up ODR with the model and data.
-        odr_model= ODR(data,linear_model,beta0=[0.,0.])
+        odr_model = ODR(data, linear_model, beta0=[0., 0.])
 
         # regression model
-        out=odr_model.run()
-        m,b=out.beta
-        return m,b
+        out = odr_model.run()
+        m, b = out.beta
+        return m, b
 
-    def predictPoint(self, line_params,sensed_point,robotpos):
-        m,b=self.points_2line(robotpos,sensed_point)
-        params1=self.lineForm_Si2G(m,b)
-        predx,predy=self.line_interesect_general(params1,line_params)
+    def predictPoint(self, line_params, sensed_point, robotpos):
+        m, b = self.points_21ine(robotpos, sensed_point)
+        params1 = self.lineForm_Si2G(m, b)
+        predx, predy = self.line_interesect_general(params1, line_params)
         return predx, predy
 
-    def seed_segment_detection(self,robot_position, break_point_ind):
-        flag=True
-        self.NP=max(0,self.NP)
-        self.SEED_SEGEMENTS=[]
+    def seed_segment_detection(self, robot_position, break_point_ind):
+        flag = True
+        self.NP = max(0, self.NP)
+        self.SEED_SEGEMENTS = []
         for i in range(break_point_ind, (self.NP-self.PMIN)):
-            predicted_points_to_draw=[]
-            j=i+self.SNUM
-            m,c=self.odr_fit(self.LASERPOINTS[i:j])
+            predicted_points_to_draw = []
+            j = i+self.SNUM
+            m, c = self.odr_fit(self.LASERPOINTS[i:j])
 
-            params=self.lineForm_Si2G(m,c)
+            params = self.lineForm_Si2G(m, c)
 
-            for k in range(i,j):
-                predicted_points=self.predictPoint(params,self.LASERPOINTS[k][0],robot_position)
+            for k in range(i, j):
+                predicted_points = self.predictPoint(
+                    params, self.LASERPOINTS[k][0], robot_position)
                 predicted_points_to_draw.append(predicted_points)
-                d1=self.dist_point2point(predicted_points, self.LASERPOINTS[k][0])
+                d1 = self.dist_point2point(
+                    predicted_points, self.LASERPOINTS[k][0])
 
-                if d1> self.DELTA:
-                    flag=False
+                if d1 > self.DELTA:
+                    flag = False
                     break
-                d2=self.dist_point2line(params, predicted_points)
+                d2 = self.dist_point2line(params, predicted_points)
 
                 if d2 > self.EPSILON:
-                    flag=False
+                    flag = False
                     break
             if flag:
-                self.LINE_PARAMS=params
-                return [self.LASERPOINTS[i:j],predicted_points_to_draw,(i,j)]
+                self.LINE_PARAMS = params
+                return [self.LASERPOINTS[i:j], predicted_points_to_draw, (i, j)]
         return False
 
     def seed_segement_growing(self, indicies, break_points):
-        line_eq=self.LINE_PARAMS
-        i,j=indicies
+        line_eq = self.LINE_PARAMS
+        i, j = indicies
         # start and end of points in line segement
-        PB,PF = max(break_points,i-1),min(j+1,len(self.LASERPOINTS)-1)
+        PB, PF = max(break_points, i-1), min(j+1, len(self.LASERPOINTS)-1)
 
-        while self.dist_point2line(line_eq,self.LASERPOINTS[PF][0]) <self.EPSILON:
-            if PF>self.NP-1:
+        while self.dist_point2line(line_eq, self.LASERPOINTS[PF][0]) < self.EPSILON:
+            if PF > self.NP-1:
                 break
             else:
-                m,b=self.odr_fit(self.LASERPOINTS[PB:PF])
-                line_eq=self.lineForm_Si2G(m,b)
+                m, b = self.odr_fit(self.LASERPOINTS[PB:PF])
+                line_eq = self.lineForm_Si2G(m, b)
 
-                POINT=self.LASERPOINTS[PF][0]
+                POINT = self.LASERPOINTS[PF][0]
 
-            PF=PF+1
-            NEXTPOINT= self.LASERPOINTS[PF][0]
-            if self.dist_point2point(POINT,NEXTPOINT)> self.GMAX:
-                    break
-            PF=PF-1
+            PF = PF+1
+            NEXTPOINT = self.LASERPOINTS[PF][0]
+            if self.dist_point2point(POINT, NEXTPOINT) > self.GMAX:
+                break
+            PF = PF-1
 
             while self.dist_point2line(line_eq, self.LASERPOINTS[PB][0]) < self.EPSILON:
-                if PB<break_points:
+                if PB < break_points:
                     break
                 else:
-                    m,b=self.odr_fit(self.LASERPOINTS[PB:PF])
-                    line_eq=self.lineForm_Si2G(m,b)
-                    POINT=self.LASERPOINTS[PB][0]
+                    m, b = self.odr_fit(self.LASERPOINTS[PB:PF])
+                    line_eq = self.lineForm_Si2G(m, b)
+                    POINT = self.LASERPOINTS[PB][0]
 
-                PB=PB-1
-                NEXTPOINT=self.LASERPOINTS[PB][0]
-                if self.dist_point2point(POINT,NEXTPOINT) > self.GMAX:
+                PB = PB-1
+                NEXTPOINT = self.LASERPOINTS[PB][0]
+                if self.dist_point2point(POINT, NEXTPOINT) > self.GMAX:
                     break
-            PB+=1
-            LR=self.dist_point2point(self.LASERPOINTS[PB][0], self.LASERPOINTS[PF][0])
-            PR=len(self.LASERPOINTS[PB:PF])
+            PB += 1
+            LR = self.dist_point2point(
+                self.LASERPOINTS[PB][0], self.LASERPOINTS[PF][0])
+            PR = len(self.LASERPOINTS[PB:PF])
 
-            if (LR>= self.LMIN) and (PR>=self.PMIN):
-                self.LINE_PARAMS=line_eq
-                m,b=self.lineForm_G2SI(line_eq[0],line_eq[1],line_eq[2])
-                self.two_points=self.line_2points(m,b)
-                self.LINE_SEGEMENTS.append((self.LASERPOINTS[PB+1][0],self.LASERPOINTS[PF-1][0]))
-                return [self.LASERPOINTS[PB:PF], self.two_points, (self.LASERPOINTS[PB+1][0],self.LASERPOINTS[PF-1][0]), PF, line_eq, (m,b)]
+            if (LR >= self.LMIN) and (PR >= self.PMIN):
+                self.LINE_PARAMS = line_eq
+                m, b = self.lineForm_G2SI(line_eq[0], line_eq[1], line_eq[2])
+                self.two_points = self.line_2points(m, b)
+                self.LINE_SEGEMENTS.append(
+                    (self.LASERPOINTS[PB+1][0], self.LASERPOINTS[PF-1][0]))
+                return [self.LASERPOINTS[PB:PF], self.two_points, (self.LASERPOINTS[PB+1][0], self.LASERPOINTS[PF-1][0]), PF, line_eq, (m, b)]
             else:
                 return False
--- main.py
+++ main.py
@@ -1,29 +1,84 @@
-import world,sensors,features
+from scipy.odr.odrpack import ODR
+import world
+import sensors
+import features
 import pygame
-environment=world.buildEnvironment((600,1200))
-environment.originalMap = environment.map.copy()
-laser=sensors.LaserSensor(200,environment.originalMap,unc=(0.5,0.01))
-environment.map.fill((255,255,255))
+import random
+
+FeatureMap = features.featuresDetection()
+environment = world.buildEnvironment((600, 1200))
+originalMap = environment.map.copy()
+laser = sensors.LaserSensor(200, originalMap, unc=(0.5, 0.01))
+environment.map.fill((255, 255, 255))
 environment.infomap = environment.map.copy()
+originalMap = environment.map.copy()
 
-running =True
 
-while running :
-    sensorON=False
+def rand_color():
+    rang = range(32, 256, 32)
+    return tuple(random.choice(rang) for _ in range(3))
+
+
+running = True
+FEATURE_DETECTION = True
+BREAK_POINT_IND = 0
+
+while running:
+    environment.infomap = originalMap.copy()
+    FEATURE_DETECTION = True
+    BREAK_POINT_IND = 0
+    ENDPOINTS = [0, 0]
+    sensorON = False
+    PREDICTED_POINTS_TODRAW = []
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running=False
-        if pygame.mouse.get_focused():
-            sensorON=True
-        elif not pygame.mouse.get_focused():
-            sensorON=False
+            running = False
+    if pygame.mouse.get_focused():
+        sensorON = True
+    elif not pygame.mouse.get_focused():
+        sensorON = False
     if sensorON:
-        position=pygame.mouse.get_pos()
-        laser.position=position
+        position = pygame.mouse.get_pos()
+        laser.position = position
         sensor_data = laser.sense_obstacles()
-        environment.dataStorage(sensor_data)
-        environment.show_sensorData()
-    environment.map.blit(environment.infomap,(0,0))
-    pygame.display.update()
-    
+        FeatureMap.laser_points_set(sensor_data)
+        while BREAK_POINT_IND < (FeatureMap.NP - FeatureMap.PMIN):
+            seedSeg = FeatureMap.seed_segment_detection(
+                laser.position, BREAK_POINT_IND)
+            if seedSeg == False:
+                break
+            else:
+                seedSegment = seedSeg[0]
+                PREDICTED_POINTS_TODRAW = seedSeg[1]
+                INDICES = seedSeg[2]
+                results = FeatureMap.seed_segement_growing(
+                    INDICES, BREAK_POINT_IND)
+                if results == False:
+                    BREAK_POINT_IND = INDICES[1]
+                    continue
+                else:
+                    line_eq = results[1]
+                    m, c = results[5]
+                    line_seg = results[0]
+                    OUTERMOST = results[2]
+                    BREAK_POINT_IND = results[3]
+
+                    ENDPOINTS[0] = FeatureMap.projection_point2line(
+                        OUTERMOST[0], m, c)
+                    ENDPOINTS[1] = FeatureMap.projection_point2line(
+                        OUTERMOST[1], m, c)
+
+                    COLOR = rand_color()
+                    for point in line_seg:
+                        environment.infomap.set_at(
+                            (int(point[0][0]), int(point[0[1]]), (0, 255, 0)))
+                        pygame.draw.circle(environment.infomap, COLOR, (int(
+                            point[0][0]), int(point[0][1])), 2, 0)
+                    pygame.draw.line(environment.infomap,
+                                     (255, 0, 0), ENDPOINTS[0], ENDPOINTS[1], 2)
+                    environment.dataStorage(sensor_data)
+                    environment.show_sensorData()
+                    environment.map.blit(environment.infomap, (0, 0))
+                    pygame.display.update()
+
 pygame.quit()
